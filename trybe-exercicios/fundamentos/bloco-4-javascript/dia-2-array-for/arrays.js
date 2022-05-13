08937b173d2680a1251ca0cefa1f7ac187269da7
Assis Berlanda de Medeiros <assisberlandamedeiros@gmail.com>
2022-05-13 15:54:02 -0300

    Exercícios Array e For

---
+++ trybe-exercicios/fundamentos/bloco-4-javascript/dia-2-array-for/arrays.js
@@ -0,0 +1,36 @@
+/*
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Lavar carro', 'Limpar casa')
+tasksList.unshift('Estudar', 'Fazer dever') 
+
+tasksList.pop();
+tasksList.shift();
+
+
+for (let i = 0; i < tasksList.length; i++) {
+    console.log(tasksList[i]);
+}
+console.log(tasksList.indexOf('Reunião'));
+*/
+// ------------------ Exercício 1 ------------------
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+*/
+// -------------------------------------------------
+// ------------------ Exercício 2 ------------------
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+*/
+// -------------------------------------------------
+// ------------------ Exercício 3 ------------------
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+console.log(menu);
+// -------------------------------------------------
